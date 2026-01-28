310a5633a275648654c8c2523ca5e0668711ac06
Ivan <kuuhakukokyutosu@gmail.com>
2026-01-28 11:27:09 +0200

    лабораторна робота 2

--- helloworld/src/main/kotlin/Main.kt
+++ helloworld/src/main/kotlin/Main.kt
@@ -1,17 +1,58 @@
+import com.diacht.ktest.compose.startTestUi
+import org.example.helloworld.BuildConfig
+import kotlin.math.*
+
 fun seed(): String = "Kokyutosu"
-fun labNumber() : Int = 1
+fun labNumber() : Int = BuildConfig.LAB_NUMBER
+
+fun iCalculate(
+    x0:Int = -95,
+    x1:Int = 97,
+    x2:Int = -17,
+    x3:Int = 114,
+    x4:Int = -77
+) : Double {
+    val x: Double = (x0 * x1 * x2 * x3 * x4).toDouble()
+    val res: Double = ln(x)
+    return res
+}
+
+fun dCalculate(
+    x0:Double = -0.32,
+    x1:Double = -27.72,
+    x2:Double = -47.73,
+    x3:Double = -26.25,
+    x4:Double = 36.04
+): Double {
+    val x:Double = cbrt(x0 * x1 * x2 * x3 * x4)
+    return x
+}
+
+fun strCalculate(
+    x0:String = "ATGCJ",
+    x1:String = "ATGCJ"
+):Int {
+
+    val half = x0.length / 2
+    var result = 0
+
+    for (i in x0.indices) {
+        val ch0 = x0[i]
+        if (ch0 == 'T' || ch0 == 'C') {
+            if (x0[i] != x1[i]) {
+                result += if (i < half) 2 else 1
+            }
+        }
+    }
+
+    return result
+}
+
 fun main(args: Array<String>) {
     println("Лабораторна робота №${labNumber()} користувача ${seed()}")
-    var kitty = "Васько"
-    kitty += " \uD83D\uDC31"
-    val age = 7
-    println("Кошеня №1 - $kitty віком $age років")
-    val catName: String = "Мурзик \uD83D\uDC08"
-    val weight: Float = 3.5f
-    println("Кошеня №2 - $catName з вагою $weight кг")
-    var kitty2 = "Рудий"
-    kitty2 += " \uD83D\uDC06"
-    val age2 = 6
-    val weight2 = 8.2f
-    println("Кошеня №3 - $kitty2 віком $age2 з вагою $weight2 кг")
+    println(iCalculate())
+    println(dCalculate())
+    println(strCalculate())
+
+    startTestUi(seed(), labNumber())
 }
